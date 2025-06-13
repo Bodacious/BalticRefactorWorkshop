14a725256bc0c10b4c9a03ac011be1b6aefb966b
Gavin Morrice <gavin@gavinmorrice.com>
2025-06-13 10:33:30 +0300

    Fix typo bug in rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
-  def self.for_product(product
+  def self.for_product(product)
     where(product_id: product.id)
   end
 end
